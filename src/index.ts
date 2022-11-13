d14e293e524d0228a18bd9087fe6964d37ae0802
Tony Keiser <keisertony@gmail.com>
2022-11-12 16:50:43 -0800

    Extracted logic into class added data conversion and types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import fs from 'fs'
+import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MatchResult'
 
-const matches = fs
-  .readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
+const reader = new CsvFileReader('football.csv')
+reader.read()
 
-console.log(matches)
+console.log(reader.data)
+
+let manUnitedWins = 0
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`)
