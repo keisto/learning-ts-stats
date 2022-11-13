3835c5fd657a25ac40fd303c052e7da3098e2ca2
Tony Keiser <keisertony@gmail.com>
2022-11-12 20:19:19 -0800

    Added HTML Reporting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { CsvFileReader } from './CsvFileReader'
 import { MatchReader } from './MatchReader'
 import { WinsAnalysis } from './analyzers/WinsAnalysis'
-import { ConsoleReport } from './ConsoleReport'
+import { HtmlReport } from './HtmlReport'
 import { Summary } from './Summary'
 
 const csvFileReader = new CsvFileReader('football.csv')
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
 
 summary.buildAndPrintReport(matchReader.matches)
